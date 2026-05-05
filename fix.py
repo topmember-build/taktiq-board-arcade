071e82c529ae4a104931561c5b7d8ef4ccef59c6
topmember-build <topmemberdidit@gmail.com>
2026-05-05 08:37:49 +0100

    chore: remove temp fix script

--- fix.py
+++
@@ -1,5 +0,0 @@
-import re 
-content = open('src/routes/match.$id.tsx', encoding='utf-8').read() 
-fixed = content.replace('@/server/matchMoves.functions"', '@/server/matchMoves.functions.server"') 
-open('src/routes/match.$id.tsx', 'w', encoding='utf-8').write(fixed) 
-print('Done') 
